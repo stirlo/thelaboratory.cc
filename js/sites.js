6f4958d729304ab24874fe68d11b81332d51a9bd
stirling campbell <3212918+stirlo@users.noreply.github.com>
2024-10-26 01:23:25 +1100

    Create sites.js
    omne stip shoppe

---
+++ js/sites.js
@@ -0,0 +1,78 @@
+// /js/sites.js
+const sites = [
+    {
+        name: 'Zmanim, Calendar and ICS',
+        url: 'https://stirlo.github.io/zmanim_ICS/',
+        github: 'stirlo/zmanim_ICS',
+        logo: 'zmanim_ICS.png'
+    },
+    {
+        name: 'Adhan-Flipperzero',
+        url: 'https://github.com/stirlo/adhan/',
+        github: 'stirlo/adhan',
+        logo: 'adhan-flipper-512x512.png'
+    },
+    {
+        name: 'Adhan Swift/Web',
+        url: 'https://stirlo.github.io/adhan-swift/',
+        github: 'stirlo/adhan-swift',
+        logo: 'adhan-swift-512x512.png'
+    },
+    {
+        name: 'InfiniteReality.cc',
+        url: 'https://infinitereality.cc',
+        github: 'jailbreaktheuniverse/infinitereality.cc',
+        logo: 'infinitereality.cc-384x384.png'
+    },
+    {
+        name: 'JPT-flipperzero',
+        url: 'https://github.com/stirlo/JPT-flipperzero/',
+        github: 'stirlo/JPT-flipperzero',
+        logo: 'JPT-flipper-512x512.png'
+    },
+    {
+        name: 'OurSquadIs.top',
+        url: 'https://oursquadis.top',
+        github: 'stirlo/oursquadis.top',
+        logo: 'oursquadis.top-512x512.png'
+    },
+    {
+        name: 'Plaintext to ICS Converter',
+        url: 'https://stirlo.github.io/txt_to_ICS_Prayer_Times/',
+        github: 'stirlo/txt_to_ICS_Prayer_Times',
+        logo: 'alarm-clock.svg'
+    },
+    {
+        name: 'Stirlo.be',
+        url: 'https://stirlo.be',
+        github: 'stirlo/stirlo.be',
+        logo: 'stirlo.be-512x512.png'
+    },
+    {
+        name: 'Stirlo.space',
+        url: 'https://stirlo.space',
+        github: 'stirlo/stirlo.space',
+        logo: 'stirlo.space-512x512.png'
+    },
+    {
+        name: 'TFP.la',
+        url: 'https://tfp.la',
+        github: 'stirlo/tfp.la',
+        logo: 'tfp-green-512.png'
+    },
+    {
+        name: 'The Goss Room',
+        url: 'https://thegossroom.com',
+        github: 'stirlo/thegossroom.com',
+        logo: 'thegossroom-512x512.png'
+    },
+    {
+        name: 'TheLaboratory.cc',
+        url: 'https://thelaboratory.cc',
+        github: 'stirlo/thelaboratory.cc',
+        logo: 'apple-touch-icon.png'
+    }
+];
+
+// Make sites available globally
+window.sites = sites;
