763bc3b38c6daf13287795a726955f5fcda7f366
stirling campbell <3212918+stirlo@users.noreply.github.com>
2024-10-27 00:30:38 +1100

    Update sites.js

--- js/sites.js
+++ js/sites.js
@@ -1,78 +1,85 @@
-// /js/sites.js
+// sites.js
 const sites = [
     {
-        name: 'Zmanim, Calendar and ICS',
-        url: 'https://stirlo.github.io/zmanim_ICS/',
-        github: 'stirlo/zmanim_ICS',
-        logo: 'zmanim_ICS.png'
+        name: "The Laboratory",
+        url: "https://thelaboratory.cc",
+        logo: "/assets/images/icons/apple-touch-icon.png",
+        github: "stirlo/thelaboratory.cc",
+        description: "Project Hub"
     },
     {
-        name: 'Adhan-Flipperzero',
-        url: 'https://github.com/stirlo/adhan/',
-        github: 'stirlo/adhan',
-        logo: 'adhan-flipper-512x512.png'
+        name: "Adhan",
+        url: "https://adhan.thelaboratory.cc",
+        logo: "/assets/images/projects/adhan-swift-512x512.png",
+        github: "stirlo/adhan",
+        description: "Prayer Times"
     },
     {
-        name: 'Adhan Swift/Web',
-        url: 'https://stirlo.github.io/adhan-swift/',
-        github: 'stirlo/adhan-swift',
-        logo: 'adhan-swift-512x512.png'
+        name: "Adhan Swift",
+        url: "https://adhan-swift.thelaboratory.cc",
+        logo: "/assets/images/projects/adhan-swift-512x512.png",
+        github: "stirlo/adhan-swift",
+        description: "Swift Prayer Times"
     },
     {
-        name: 'InfiniteReality.cc',
-        url: 'https://infinitereality.cc',
-        github: 'jailbreaktheuniverse/infinitereality.cc',
-        logo: 'infinitereality.cc-384x384.png'
+        name: "JPT Flipper Zero",
+        url: "https://jpt-flipper.thelaboratory.cc",
+        logo: "/assets/images/projects/JPT-flipper-512x512.png",
+        github: "stirlo/JPT-flipperzero",
+        description: "Flipper Zero Tools"
     },
     {
-        name: 'JPT-flipperzero',
-        url: 'https://github.com/stirlo/JPT-flipperzero/',
-        github: 'stirlo/JPT-flipperzero',
-        logo: 'JPT-flipper-512x512.png'
+        name: "Infinite Reality",
+        url: "https://infinitereality.cc",
+        logo: "/assets/images/projects/infinitereality.cc-384x384.png",
+        description: "Virtual Reality Hub"
     },
     {
-        name: 'OurSquadIs.top',
-        url: 'https://oursquadis.top',
-        github: 'stirlo/oursquadis.top',
-        logo: 'oursquadis.top-512x512.png'
+        name: "Our Squad Is Top",
+        url: "https://oursquadis.top",
+        logo: "/assets/images/projects/oursquadis.top-512x512.png",
+        description: "Gaming Community"
     },
     {
-        name: 'Plaintext to ICS Converter',
-        url: 'https://stirlo.github.io/txt_to_ICS_Prayer_Times/',
-        github: 'stirlo/txt_to_ICS_Prayer_Times',
-        logo: 'alarm-clock.svg'
+        name: "Stirlo.be",
+        url: "https://stirlo.be",
+        logo: "/assets/images/projects/stirlo.be-512x512.png",
+        description: "Personal Site"
     },
     {
-        name: 'Stirlo.be',
-        url: 'https://stirlo.be',
-        github: 'stirlo/stirlo.be',
-        logo: 'stirlo.be-512x512.png'
+        name: "Stirlo.space",
+        url: "https://stirlo.space",
+        logo: "/assets/images/projects/stirlo.space-512x512.png",
+        description: "Project Space"
     },
     {
-        name: 'Stirlo.space',
-        url: 'https://stirlo.space',
-        github: 'stirlo/stirlo.space',
-        logo: 'stirlo.space-512x512.png'
+        name: "The Goss Room",
+        url: "https://thegossroom.com",
+        logo: "/assets/images/projects/thegossroom-512x512.png",
+        description: "News Aggregator"
     },
     {
-        name: 'TFP.la',
-        url: 'https://tfp.la',
-        github: 'stirlo/tfp.la',
-        logo: 'tfp-green-512.png'
-    },
-    {
-        name: 'The Goss Room',
-        url: 'https://thegossroom.com',
-        github: 'stirlo/thegossroom.com',
-        logo: 'thegossroom-512x512.png'
-    },
-    {
-        name: 'TheLaboratory.cc',
-        url: 'https://thelaboratory.cc',
-        github: 'stirlo/thelaboratory.cc',
-        logo: 'apple-touch-icon.png'
+        name: "Zmanim",
+        url: "https://zmanim.thelaboratory.cc",
+        logo: "/assets/images/projects/alarm-clock.svg",
+        github: "stirlo/zmanim",
+        description: "Jewish Prayer Times"
     }
 ];
 
-// Make sites available globally
-window.sites = sites;
+// Helper function to get site by URL
+function getSiteByUrl(url) {
+    return sites.find(site => site.url === url);
+}
+
+// Helper function to get site by name
+function getSiteByName(name) {
+    return sites.find(site => site.name === name);
+}
+
+// Export for use in other scripts
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = { sites, getSiteByUrl, getSiteByName };
+} else {
+    window.laboratorySites = { sites, getSiteByUrl, getSiteByName };
+}
